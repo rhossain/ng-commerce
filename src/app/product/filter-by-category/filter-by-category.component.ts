bd8fe0726399664d1c66a8322d4dca6f60be8a48
Md. Sazzad Hossain <sazzad.hossain@bs-23.net>
2025-05-13 16:32:39 +0600

    feat: adds initial components

---
+++ src/app/product/filter-by-category/filter-by-category.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-by-category',
+  standalone: true,
+  imports: [],
+  templateUrl: './filter-by-category.component.html',
+  styleUrl: './filter-by-category.component.scss'
+})
+export class FilterByCategoryComponent {
+
+}
