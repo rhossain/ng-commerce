315d9622fef8017f6c81919870b77ac4b5ec9c35
Md. Sazzad Hossain <sazzad.hossain@bs-23.net>
2025-05-13 16:45:30 +0600

    feat: updates services and installed ngx-toastr

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,9 +1,61 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { ProductModel, ProductVariant } from '../models/product.model';
+import { CartItem } from '../models/cart.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  private cartKey = 'cart';
+  private cartSubject = new BehaviorSubject<number>(0);
+  cart$: Observable<number> = this.cartSubject.asObservable();
 
-  constructor() { }
+  constructor(private toastrService: ToastrService) { }
+
+  private saveCart(cart: CartItem[]):void {
+    localStorage.setItem(this.cartKey, JSON.stringify(cart));
+  }
+
+  getCart(): CartItem[] {
+    const cartData = localStorage.getItem(this.cartKey);
+    const cart: CartItem[] = cartData ? JSON.parse(cartData) : [];
+    this.cartSubject.next(cart.length);
+    return cart;
+  }  
+
+  addToCart(product: ProductModel, variant: ProductVariant): void {
+    const cart = this.getCart();
+  
+    const existingItem = cart.find(
+      (item) =>
+        item.product.id === product.id && item.variant.id === variant.id
+    );
+  
+    if (existingItem) {
+      existingItem.quantity += 1;
+    } else {
+      cart.push({ product, variant, quantity: 1 });
+    }
+  
+    this.saveCart(cart);
+    this.cartSubject.next(cart.length);
+    this.toastrService.success(`Added ${product.name} to cart`, `Cart updated`);
+  }  
+
+  clearCart():void {
+    this.saveCart([]);
+  }
+
+  removeFromCart(productId: number, variantId: number): void {
+    const updatedCart = this.getCart().filter(
+      (item: CartItem) =>
+        !(item.product.id === productId && item.variant.id === variantId)
+    );
+  
+    this.saveCart(updatedCart);
+    this.cartSubject.next(updatedCart.length);
+    this.toastrService.info(`Item removed from cart`, `Cart updated`);
+  }  
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,42 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { ProductModel, ProductResponse } from '../models/product.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  private apiUrl: string;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.apiUrl = environment.apiBaseUrl;
+  }
+
+  getProduct(id: number): Observable<ProductModel> {
+    const url = `${this.apiUrl}/${environment.apiEndpoints.product.getProduct}/${id}`;
+    return this.http.get<ProductModel>(url);
+  }
+
+  getProducts(
+    page: number = 1,
+    perPage: number = 1,
+    sortBy: string = 'id',
+    orderBy: 'asc' | 'desc' = 'asc',
+    categoryId?: number | null,
+    limit?: number,
+    minPrice?: number | null,
+    maxPrice?: number | null
+  ): Observable<ProductResponse> {
+    let url = `${this.apiUrl}/${environment.apiEndpoints.product.getAllProduct}?page=${page}&perPage=${perPage}&sortBy=${sortBy}&orderBy=${orderBy}`;
+
+    if (categoryId) url += `&categoryId=${categoryId}`;
+    if (limit) url += `&perPage=${limit}`;
+    if (minPrice != null) url += `&minPrice=${minPrice}`;
+    if (maxPrice != null) url += `&maxPrice=${maxPrice}`;
+    console.log('Final product API URL:', url);
+    // console.log(url);
+    return this.http.get<ProductResponse>(url);
+  }
 }
