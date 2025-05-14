bf2125cfff711fa402032320c63af32b2fcbac2d
Md. Sazzad Hossain <sazzad.hossain@bs-23.net>
2025-05-14 18:05:19 +0600

    feat: install and configure fontawesome

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export default class HomeComponent {
-
+  faHouse = faHouse;
 }
