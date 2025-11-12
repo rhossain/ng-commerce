12a33fc202a7e3735eb93d1b614a1d58227da109
Md. Sazzad Hossain <sazzad.hossain@bs-23.net>
2025-11-12 16:01:48 +0600

    Deploy to ngcommerce - 2025-11-12 16:01:48 from development

---
+++ chunk-WLEC2QAF.js
@@ -0,0 +1,1 @@
+import"./chunk-4CLCTAJ7.js";var o=[{path:"",pathMatch:"full",redirectTo:"shop"},{path:"shop",loadComponent:()=>import("./chunk-RSK2LFTH.js")},{path:"search",loadComponent:()=>import("./chunk-RUGWPBXO.js")},{path:"detail/:productId",loadComponent:()=>import("./chunk-3WMA5PZO.js")}],t=o;export{t as default};
